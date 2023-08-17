25d33b92d82717e7dfdc0a834f4276b45c374f2c
jcbalbdev <jcbalbdev@gmail.com>
2023-08-16 20:14:05 -0500

    segundo commit

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -5,7 +5,7 @@ console.log('leyendo el primer archivo..')
 const textUno = fs.readFileSync('./archivo.txt','utf-8')
 console.log(textUno)
 
-console.log('haciendo cosas mientras lee el primer archivo')
+console.log('haciendo cosas mientras lee el primer archivo...')
 
 console.log('leyendo el segundo archivo..')
 const textDos = fs.readFileSync('./archivo2.txt','utf-8')
